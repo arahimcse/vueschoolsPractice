ae56efc98b65581e5872a77b3684b5616d8a3a13
Abdur Rahim <arahimcse@gmail.com>
2023-07-19 10:07:37 +0600

    v-if and template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue';
+import ListRendering from './components/listRendering.vue';
 const msg = ref("Hello World")
 
 </script>
@@ -7,6 +8,8 @@ const msg = ref("Hello World")
 <template>
   <h1>{{ msg }}</h1>
   <input v-model="msg">
+
+  <ListRendering/>
 </template>
 
 <style scoped>
---
+++ src/components/listRendering.vue
@@ -0,0 +1,10 @@
+<script setup>
+const ifBoolean = true
+const title = 'title'
+</script>
+
+<template v-if="ifBoolean">
+    <h1>{{ title }}</h1>
+    <p >paragraph 1</p>
+    <p>paragraph 2</p>
+</template>
