6c6b3a019fc0d7caec766deb01c74ace502cb321
Abdur Rahim <arahimcse@gmail.com>
2023-07-20 23:03:22 +0600

    take pure object of current event element

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import CallingMethodinLineHandler from './components/callingMethodinLineHandler.vue';
+import accessingEventArgumentInLineHandler from './components/accessingEventArgumentInLineHandler.vue';
 import inlineHandler from './components/inlineHandler.vue'
 import methodHandler from './components/methodHandler.vue'
 
@@ -12,9 +13,10 @@ import methodHandler from './components/methodHandler.vue'
 <methodHandler/>
 
 <h1>Calling Methods in Inline Handlers</h1>
-
 <CallingMethodinLineHandler/>
 
+<h1>Accessing Event Argument in Inline Handlers</h1>
+<accessingEventArgumentInLineHandler/>
 </template>
 
 <style scoped>
---
+++ src/components/accessingEventArgumentInLineHandler.vue
@@ -0,0 +1,27 @@
+<script setup>
+function warn(message, event) {
+  // now we have access to the native event
+  console.log(event)
+  if (event) {
+    event.preventDefault()
+  }
+  alert(message)
+}
+</script>
+
+<template>
+<!-- using $event special variable -->
+<button @click="warn('Form cannot be submitted yet.', $event)">
+  Submit
+</button>
+
+<!-- using inline arrow function -->
+<button @click="(event) => warn('Form cannot be submitted yet.', event)">
+  Submit
+</button>
+
+</template>
+
+<style>
+
+</style>
