9d7644a5062703fecdc70d570118b2f243ade9cb
Abdur Rahim <arahimcse@gmail.com>
2023-07-20 15:06:10 +0600

    vue 3 event handler

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import EventHandler from './components/eventHandler.vue';
+
+
+</script>
+
+<template>
+<EventHandler/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/eventHandler.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<template>
+ 
+ <button @click="count++">Count</button>
+ <p>{{ count }}</p>
+</template>
+
+<style scoped>
+
+</style>
