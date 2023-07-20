7b18fc91a8fa35e34b75632ab944d4943e8cb308
Abdur Rahim <arahimcse@gmail.com>
2023-07-20 07:31:14 +0600

    v-for used in template tag

--- src/components/listRendering.vue
+++ src/components/listRendering.vue
@@ -10,10 +10,19 @@ const myObject = reactive({
 
 <template>
     <ul>
-  <li v-for="value in myObject">
+  <li v-for="value in myObject" >
     {{ value }}
   </li>
 </ul>
+
+<ul>
+  <template v-for="item in 5">
+    <h1>rahim</h1>
+    <p>{{ item }}</p>
+  </template>
+</ul>
+
+
 </template>
 
 
