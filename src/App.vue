af29b516219d0060da833bbb99dc2934ba213a64
Abdur Rahim <arahimcse@gmail.com>
2023-07-21 08:04:39 +0600

    event apisode 01

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Episode01 from './components/episode01.vue';
+
+
+</script>
+
+<template>
+ <Episode01/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/episode01.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+
+const location = ref("Dhaka")
+
+const changelocation =(newLocation) => location.value = newLocation
+
+</script>
+
+<template>
+    <div class="container">
+    <button type="button" @click="changelocation('Dhaka')" class="btn btn-primary m-2">Dhaka</button>
+    <button type="button" @click="changelocation('Rajshahi')" class="btn btn-primary m-2">Rajshahi</button>
+    <button type="button" @click="changelocation('Khulna')" class="btn btn-primary m-2">Khulna</button>
+  </div>
+  <div class="container">
+    <div class="row">
+        <p>{{ location }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
