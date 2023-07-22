06b829dcaf884d2e389e63e53e355406de211ab7
Abdur Rahim <arahimcse@gmail.com>
2023-07-22 09:15:53 +0600

    episode 06

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
+import Episode06 from './components/episode06.vue';
+
 // import Episode01 from './components/episode01.vue';
 // import Episode02 from './components/episode02.vue';
-import Episode03 from './components/episode03.vue';
+// import Episode03 from './components/episode03.vue';
+// import Episode04 from './components/episode04.vue';
 
 
 </script>
@@ -9,7 +12,9 @@ import Episode03 from './components/episode03.vue';
 <template>
  <!-- <Episode01/> -->
 <!-- <Episode02/> -->
-<episode03/>
+<!-- <episode03/> -->
+<!-- <episode04/> -->
+<episode06/>
 </template>
 
 <style scoped>
