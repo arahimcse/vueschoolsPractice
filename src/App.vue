2575d29c300745bbc0c8d6c37dcf07e2354853d8
Abdur Rahim <arahimcse@gmail.com>
2023-07-24 21:15:03 +0600

    data send from parent component to child component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 import ListRendering from './components/listRendering.vue';
+import GetDataFromParentComponent from './components/getDataFromParentComponent.vue';
 const msg = ref("Hello World")
+
+/**
+ * Pass data to the child component
+ */
+const todoLists = reactive([
+  {id:0,title:"Vagetal"},
+  {id:1,title:"Banana"},
+  {id:2,title:"Apple"}
+])
 
 </script>
 
@@ -18,6 +28,14 @@ const msg = ref("Hello World")
   <h1 v-if="item%2==0"> {{ item  }}</h1>
  </span>
  
+ <ol>
+
+  <GetDataFromParentComponent
+  v-for="item in todoLists"
+  :todo="item"
+  :key="item.id">
+</GetDataFromParentComponent>
+ </ol>
 
 </template>
 
---
+++ src/components/getDataFromParentComponent.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+    todo: Object
+})
+</script>
+
+<template>
+    <li>{{ todo.title }}</li>
+</template>
+
+<style scoped>
+
+</style>
