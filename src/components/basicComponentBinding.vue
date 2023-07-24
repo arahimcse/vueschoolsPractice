28e1b05f14c06cfd6b164044ad2b666458aa1192
Abdur Rahim <arahimcse@gmail.com>
2023-07-24 20:56:51 +0600

    vue component

---
+++ src/components/basicComponentBinding.vue
@@ -0,0 +1,29 @@
+<!-- Component Building Step -->
+
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+
+
+/**
+ * Component not building step
+ * export default {
+  setup() {
+    const count = ref(0)
+    return { count }
+  },
+  template: `
+    <button @click="count++">
+      You clicked me {{ count }} times.
+    </button>`
+  // Can also target an in-DOM template:
+  // template: '#my-template-element'
+}
+ */
+
+</script>
+
+<template>
+  <button @click="count++">You clicked me {{ count }} times.</button>
+</template>
+
