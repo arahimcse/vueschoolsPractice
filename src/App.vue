67234b1cd1ddf138ac2494545a8db481ec525c55
Abdur Rahim <arahimcse@gmail.com>
2023-08-09 22:00:40 +0600

    update

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script setup>
-import Episode07 from './components/episode07.vue';
 
-// import Episode01 from './components/episode01.vue';
-// import Episode02 from './components/episode02.vue';
-// import Episode03 from './components/episode03.vue';
-// import Episode04 from './components/episode04.vue';
-// import Episode05 from './components/episode05.vue';
-// import episode05Toggle from './components/episode05Toggle.vue';
-// import Episode06 from './components/episode06.vue';
+
+
+import Episode01 from './components/episode01.vue';
+import Episode02 from './components/episode02.vue';
+import Episode03 from './components/episode03.vue';
+import Episode04 from './components/episode04.vue';
+import Episode05 from './components/episode05.vue';
+import episode05Toggle from './components/episode05Toggle.vue';
+import Episode06 from './components/episode06.vue';
+import Episode07 from './components/episode07.vue';
+import Episode08 from './components/episode08.vue';
+import episode09 from './components/episode09.vue';
 
 </script>
 
@@ -18,8 +22,10 @@ import Episode07 from './components/episode07.vue';
 <!-- <episode04/> -->
 <!-- <episode05/> -->
 <!-- <episode05Toggle/> -->
-<!-- <episode06/> -->
-<Episode07/>
+<episode06/>
+<!-- <Episode07/> -->
+<!-- <Episode08/> -->
+<!-- <episode09/> -->
 </template>
 
 <style scoped>
---
+++ src/components/episode09.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref, reactive } from 'vue';
+const lights = reactive([
+    {
+        title:"Light 1 ",
+        on:"//img.icons8.com/?size=512&id=12244&format=png",
+        off:"//img.icons8.com/?size=512&id=75&format=png",
+        isOn: true
+    },
+    {
+        title:"Light 2 ",
+        on:"//img.icons8.com/?size=512&id=12244&format=png",
+        off:"//img.icons8.com/?size=512&id=75&format=png",
+        isOn: true
+    },
+    {
+        title:"Light 3 ",
+        on:"//img.icons8.com/?size=512&id=12244&format=png",
+        off:"//img.icons8.com/?size=512&id=75&format=png",
+        isOn: false
+    }
+])
+const bgBlue = "bg-blue-700"
+const bgGray = "bg-gray-700"
+</script>
+
+<template>
+  <section class="container mx-auto flex items-center flex-col">
+        <h1 class="text-center text-2xl py-10">Assignment</h1>
+        <p class="p-5">Make These Buttons Work</p>
+        <div class="flex justify-between space-x-6">
+            <div class="p-10 border w-96 flex flex-col space-y-5">
+                <button v-for="(light, index) in lights" :key="index" @click="light.isOn =!light.isOn" :class="light.isOn?bgBlue:bgGray" class="hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded">
+                    {{ light.title }} {{ light.isOn?"On":"Off" }}
+                </button>
+            </div>
+            <div class="p-10 border w-96 flex flex-col space-y-5">
+                <img class="w-20" v-for="(light, index) in lights" :src="light.isOn?light.on:light.off"  :alt="light.title" :key="index">
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped></style>
