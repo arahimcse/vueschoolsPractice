6a976496c8415c1f89c6e49ceef4d67d380eba16
Abdur Rahim <arahimcse@gmail.com>
2023-08-12 15:09:04 +0600

    Images Carousel and Pie Chart

---
+++ vueChart/src/components/Carousel.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { onMounted, onBeforeUnmount } from 'vue';
+let carousel = null;
+const items = [
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/submerged.jpg",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/one-world-trade.jpg",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/drizzle.jpg",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg",
+];
+onMounted(() => {
+    carousel = new Flickity('#imageCarousel', {
+        autoPlay: 1500
+    })
+})
+onBeforeUnmount(() => {
+    if(carousel)
+    {
+        carousel.destroy()
+    }
+})
+</script>
+
+<template>
+    <h2 class="my-5 text-3xl">Image Carosuels </h2>
+    <div class="container carousel-container mx-auto w-5/6 mb-10" id="imageCarousel">
+        <div v-for="(item, index) in items" :style="`background-image:url(${item})`" class="carousel-items">{{ index + 1 }}
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.carousel-container {
+    width: 600px;
+    height: 400px;
+}
+
+.carousel-items {
+    width: 600px;
+    height: 400px;
+    background-color: darkgray;
+    background-size: cover;
+}
+</style>
---
+++ vueChart/src/components/PieChart.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from "vue";
+import Chart from "chart.js/auto";
+const pieChartCanvas = ref(null);
+let chartInstance = null;
+const dataset = [12, 19, 6, 8, 16, 5];
+const data = {
+    datasets: [
+        {
+            label: "Data",
+            data: dataset,
+            backgroundColor: [
+                "rgb(255, 99, 132)",
+                "rgb(54, 162, 235)",
+                "rgb(255, 205, 86)",
+            ],
+            hoverOffset: 4,
+        },
+    ],
+};
+const config = {
+    type: "pie",
+    data: data,
+};
+const createChart = () => {
+    if (pieChartCanvas.value) {
+        chartInstance = new Chart(pieChartCanvas.value, config);
+    }
+};
+onMounted(() => {
+    createChart();
+});
+onBeforeUnmount(() => {
+    if (chartInstance) {
+        chartInstance.destroy();
+    }
+});
+</script>
+
+<template>
+    <h2 class="my-5 text-3xl">Pie Chart</h2>
+    <div>
+        <canvas ref="pieChartCanvas"></canvas>
+    </div>
+</template>
+<style scoped></style>
