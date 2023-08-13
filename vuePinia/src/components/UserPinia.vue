7c5f689202c19c4aef766cda851037f588b3c98c
Abdur Rahim <arahimcse@gmail.com>
2023-08-13 11:53:51 +0600

    vue pinia

---
+++ vuePinia/src/components/UserPinia.vue
@@ -0,0 +1,14 @@
+<!-- Components name always PascalCase. Example: UsedPinia.vue -->
+<script setup>
+import { useUserStore } from '../stores/userStore'
+const userStore = useUserStore()
+</script>
+
+<template>
+    <h3>Count: {{ userStore.count }} , Double count (by getters) : {{ userStore.doubleCount }}, Forth Count(by getters and actions): {{ userStore.forthCount() }} </h3>
+    <button @click="userStore.increment()">Increment</button>
+</template>
+
+<style scoped>
+    
+</style>
